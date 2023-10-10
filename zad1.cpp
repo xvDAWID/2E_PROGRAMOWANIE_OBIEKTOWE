b4d37dd5d5a2e867084639a0a616b8c0d7a50f02
xvDAWID <146839477+xvDAWID@users.noreply.github.com>
2023-10-10 14:54:25 +0200

    10.10.2023 zadanie 1 i 2

---
+++ zad1.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string username = "admin";
+    string password = "12345";
+
+    cout << "Podaj nazwe uzytkownika: ";
+    string inputUsername;
+    cin >> inputUsername;
+
+    cout << "Podaj haslo";
+    string inputPassword;
+    cin >> inputPassword;
+
+    if (inputUsername == username && inputPassword == password) {
+        cout << "Dostęp przyznany. " << username << "!" << endl;
+
+    }
+    else {
+        cout << "Dostęp nie przyznany. " << endl;
+    }
+    return 0;
+}
