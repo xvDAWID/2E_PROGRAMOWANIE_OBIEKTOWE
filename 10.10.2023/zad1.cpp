347e2a1b9d87c10ca637037c1a8e1fe114464bd5
xvDAWID <146839477+xvDAWID@users.noreply.github.com>
2023-10-26 12:33:56 +0200

    Add files via upload

---
+++ 10.10.2023/zad1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string username = "admin";
+	string password = "12345";
+
+	cout << "Podaj nazwe uzytkownika: ";
+	string inputUsername;
+	cin >> inputUsername;
+
+	cout << "Podaj haslo: ";
+	string inputPassword;
+	cin >> inputPassword;
+
+	if (inputUsername == username && inputPassword == password) {
+		cout << "Dostep przyznany " << username << "!" << endl;
+	} else{
+		cout << "Nie przyznano dostepu. " << endl;
+	}
+	return 0;
+
+
+
+}
---
+++ 10.10.2023/zad2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+	map<string, string>userPasswords;
+
+	userPasswords["admin"] = "12345";
+	userPasswords["Kuba"] = "54321";
+	userPasswords["Nikodem"] = "69420";
+
+	bool loggedIn = false;
+	string username;
+	string password;
+
+	while (!loggedIn) {
+		cout << "Pdodaj nazwe uzytkownika: ";
+		cin >> username;
+
+		cout << "Podaj haslo: ";
+		cin >> password;
+
+		if (userPasswords.find(username) != userPasswords.end() && userPasswords[username] == password) {
+			cout << "Dostep udzielony. " << username << "!" << endl; loggedIn = true;
+		}
+		else {
+			cout << "Nie przydzielono dostepu. " << endl;
+		}
+	}
+	return 0;
+}
